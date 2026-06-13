c5792c8a9b848254fc51614715fa24a5db1ef09c
Shubhangi Thakur <shubhangithakur2404@gmail.com>
2026-06-13 23:34:31 +0530

    chore: dynamically initialize auth loading state based on localStorage to prevent login flash

--- Frontend/src/features/auth/auth.context.jsx
+++ Frontend/src/features/auth/auth.context.jsx
@@ -7,7 +7,9 @@ export const AuthContext = createContext()
 export const AuthProvider = ({ children }) => { 
 
     const [user, setUser] = useState(null)
-    const [loading, setLoading] = useState(true)
+    const [loading, setLoading] = useState(() => {
+        return localStorage.getItem("isLoggedIn") === "true"
+    })
 
     
 
